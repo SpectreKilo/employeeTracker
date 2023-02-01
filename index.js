dff1bd3afb28de1e5ca7301279d370bf759526fe
SpectreKilo <johnho8788@gmail.com>
2023-02-01 00:10:06 -0500

    added askQuestions

--- index.js
+++ index.js
@@ -12,3 +12,45 @@ db.connect((err) => {
     askQuestions();
 });
 
+const askQuestions = () => {
+    inquirer.createPromptModule(menuQuestions)
+        .then((answers) => {
+            switch (answers.firstMenu) {
+
+                case "View All Employees":
+                    viewEmployees()
+                    break;
+
+                case "Add Employee":
+                    addNewEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+
+                case "View All Roles":
+                    viewRoles();
+                    break;
+    
+                case "Add Role":
+                    addNewRole();
+                    break;
+    
+                case "View All Departments":
+                    viewDepartments();
+                    break;
+    
+                case "Add Department":
+                    addNewDepartment();
+                    break;
+    
+                case "Quit":
+                    console.log("Thank you for using Employee-Tracker")
+                    break;
+            }
+            if (answers.firstMenu === "Quit") {
+                return;
+            }
+        });
+};
